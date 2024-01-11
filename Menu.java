85d1fa0b5e665528e3e97411654925ba39ac12ca
kimpangya <97017924+kimpangya@users.noreply.github.com>
2024-01-12 00:56:25 +0900

    Update Menu.java

--- Menu.java
+++ Menu.java
@@ -1,2 +1,12 @@
+import java.lang.String;
+
 public class Menu {
+    int id=1;
+    String name;
+    String exp;
+    public Menu(String name, String exp){
+        this.id++;
+        this.name=name;
+        this.exp=exp;
+    }
 }
